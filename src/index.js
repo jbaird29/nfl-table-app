c7c08cea55ea4edd9137cf4ece04df0adc8fc3fa
jonbaird <jbaird29@gmail.com>
2021-03-29 19:36:47 -0400

    add firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './components/1unused/reportWebVitals';
+import firebase from "firebase/app";
+import "firebase/analytics";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDrjcf8G_KlYoNqmms7pwPZmGYGIk0uUds",
+  authDomain: "nfl-table.firebaseapp.com",
+  projectId: "nfl-table",
+  storageBucket: "nfl-table.appspot.com",
+  messagingSenderId: "290686633807",
+  appId: "1:290686633807:web:6a9f51ae91451dac2ea73a",
+  measurementId: "G-7HEN34YMG0"
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
